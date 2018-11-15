3cdb970b9709ee935df28a66453fd47ad8979675
Bryan Saldivar <bsaldivar.emc2@gmail.com>
2018-11-15 14:27:31 -0500

    Video and audio mix finished

--- mp4_mix_video_audio.sh
+++ mp4_mix_video_audio.sh
@@ -2,10 +2,27 @@
 video=${1}
 audio=${2}
 
-##
-#Some line
-##
-tmp_video="tmp.mp4"
+function file_exists()
+{
+  if [ ! -f ${1} ]
+  then
+    echo "No such file: "${1}". Terminating script."
+    exit
+  fi
+}
+
+
+if [ -z ${video} ] | [ -z ${audio} ]
+then
+  echo "No video or audio file specified."
+  echo "two arguments required, video_file audio_file"
+  exit
+fi
+
+file_exists ${video}
+file_exists ${audio}
+
+tmp_video=".tmp.mp4"
 output="proc_"${video}
 MP4Box ${video} -single 1 -out ${tmp_video}
 MP4Box ${tmp_video} -add ${audio}
